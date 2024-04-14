3a89a85214d2a8e22b9dcd04bc988ba6b5552db3
Anand Krishna Mishra <160732081+amishr87@users.noreply.github.com>
2024-04-13 21:08:22 -0700

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i = 0; i < nums.size(); i++)
+        {
+            for(int j = i+1; j < nums.size(); j++)
+            {
+                if(nums[j] == (target - nums[i]))
+                {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
