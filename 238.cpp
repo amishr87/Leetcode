a2580dd124406d91998e037bf9330c799c94b9a9
Anand Krishna Mishra <160732081+amishr87@users.noreply.github.com>
2024-12-26 18:58:56 +0530

    Create 238.cpp

---
+++ 238.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> answer(n, 1);
+
+        int left = 1;
+        for (int i = 0; i < n; i++) {
+            answer[i] *= left;
+            left *= nums[i];
+        }
+
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] *= right;
+            right *= nums[i];
+        }
+
+        return answer;        
+    }
+};
